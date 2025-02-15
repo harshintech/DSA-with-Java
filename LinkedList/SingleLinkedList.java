95e0a7fd0863d67e8d1a8f9d9ea776ffffd8ffae
Harsh Gohil <130136657+harshgohil02006@users.noreply.github.com>
2025-02-15 18:48:38 +0530

    Update SingleLinkedList.java

--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -147,6 +147,38 @@ class LL{
         }
         System.out.println("null");
     }
+
+     public void reverseIterate(){
+
+        if(head == null || head.next ==null){
+            return;
+        }
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode != null){
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+
+            //update
+            prevNode = currNode;
+            currNode = nextNode; 
+        }
+
+        head.next = null;
+        head = prevNode;
+    }
+
+    public Node reverseRecursive(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node newHead = reverseRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+
+    }
 }
 
 public class SingleLinkedList {
@@ -155,17 +187,23 @@ public class SingleLinkedList {
 
         LL list = new LL();
         System.out.println("After Inserte");
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.addFirst(23);
-        list.addFirst(12);
-        list.addAtPosition(3,333);
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(4);
+        list.addLast(5);
+        list.addFirst(100);
+        list.addLast(999);
+        list.addAtPosition(3,3);
         list.printLinkedList();
 
         System.out.println("After Delete");
         list.removeFirst();
         list.removeLast();
+        list.printLinkedList();
+        // list.reverseIterate();
+        list.head = list.reverseRecursive(list.head);
+
+        System.out.println("After Reverse");
         list.printLinkedList();
 
         System.out.println("Size : " + list.getSize());
