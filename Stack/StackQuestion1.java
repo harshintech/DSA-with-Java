97bba29cf5bce918f2ccb7e087e6d6e5e5f6386c
harshintech <harshgohil.coding@gmail.com>
2025-02-18 22:19:06 +0530

    add file

---
+++ Stack/StackQuestion1.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class StackQuestion1 {
+
+    public static void pushAtBottom(int data, Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(data,s);
+        s.push(top);
+
+    }
+    public static void main(String[] args){
+        Stack<Integer> s4 = new Stack<>();
+
+        s4.push(10);
+        s4.push(20);
+        s4.push(30);
+        s4.push(40);
+        s4.push(50);
+
+        pushAtBottom(33,s4);
+
+        while(!s4.isEmpty()){
+            System.out.println(s4.peek());
+            s4.pop();
+        }
+    }
+    
+}
