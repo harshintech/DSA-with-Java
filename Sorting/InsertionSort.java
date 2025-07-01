35d3e3adc84d4da505c3d0b7065fb845aec370d0
harshintech <harshgohil.coding@gmail.com>
2025-07-01 10:10:52 +0530

    add notes

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,3 +1,25 @@
+// NOTES :
+// 📌 When to use Insertion Sort
+//     -> When the array is small.
+//     -> When the array is almost sorted — it works very well in this case.
+
+// | Case                            | Time Complexity |
+// | ------------------------------- | --------------- |
+// | **Best Case** (already sorted)  | **O(n)**        |
+// | **Average Case**                | **O(n²)**       |
+// | **Worst Case** (reverse sorted) | **O(n²)**       |
+
+
+// ⚡️ Why best case is O(n)?
+//  -> If the array is already sorted, each new element just needs 1 comparison.
+//  -> So, only 1 pass → linear time.
+
+
+// ✅ Key takeaway:
+// Insertion Sort is simple, stable, and great for small or nearly sorted arrays, but not good for large unsorted data.
+
+
+
 public class InsertionSort {
  public static void main(String[] args) {
     int arr[] = {1,5,2,56,34,47,67,23,8};
