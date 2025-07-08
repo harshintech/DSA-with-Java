7226905100eb9edc8238666309868c7aa186c0c8
harshintech <harshgohil.coding@gmail.com>
2025-07-08 18:28:29 +0530

    add notes

--- Backtracking/Backtracking.java
+++ Backtracking/Backtracking.java
@@ -1,18 +1,19 @@
+// Time Complexity : O(n*n!);
+
 public class Backtracking {
     public static void printPermutation(String str,String perm,int idx){
         if(str.length() == 0){
             System.out.println(perm);
-            // System.out.println();
+            System.out.println();
             return;
         }
 
         for(int i = 0;i<str.length();i++){
             char currChar = str.charAt(i);
-            // System.out.println("currChar : " + currChar);
+            System.out.println("currChar : " + currChar);
             String newStr = str.substring(0, i) +str.substring(i+1);
-            // System.out.println("newStr : " + newStr);
-
-            // System.out.println(newStr+ " " + (perm+currChar) + " " +(idx+1));
+            System.out.println("newStr : " + newStr);
+            System.out.println(newStr+ " " + (perm+currChar) + " " +(idx+1));
 
 
             printPermutation(newStr, perm+currChar, idx+1);
