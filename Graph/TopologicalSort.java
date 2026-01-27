68291937e14ee0cd69347b785c983fd36493d19e
harshintech <harshgohil.coding@gmail.com>
2026-01-27 22:33:05 +0530

    added topology sort

--- Graph/TopologicalSort.java
+++ Graph/TopologicalSort.java
@@ -1,6 +1,7 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class TopologicalSort {
       static class Edge{
@@ -19,20 +20,64 @@ public class TopologicalSort {
             graph[i] = new ArrayList<Edge>();
         }
 
-        graph[0].add(new Edge(0,2)); 
+        /*
+        (5) ------> (0) <------ (4)
+         |                       |
+         v                       v
+        (2) ------> (3) ------> (1)
+         */
 
-        graph[1].add(new Edge(1,0));
+        graph[2].add(new Edge(2,3)); 
 
-        graph[2].add(new Edge(2,3));  
+        graph[3].add(new Edge(3,1));
+
+        graph[4].add(new Edge(4,0)); 
+        graph[4].add(new Edge(4,1));  
+
         
-        graph[3].add(new Edge(3,0));  //when you comment out this line means this is not direct any one this last;
+        graph[5].add(new Edge(5,0));  //when you comment out this line means this is not direct any one this last;
+        graph[5].add(new Edge(5,2));  
 
     }
+
+    public static void topSortUtil(ArrayList<Edge> graph[],int curr,boolean vis[],Stack<Integer> stack){
+        vis[curr] = true;
+
+
+        for(int i = 0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i); 
+
+            if(!vis[e.dest]){
+                topSortUtil(graph,e.dest,vis,stack);
+            }
+        }
+
+        stack.push(curr);
+    }
+
+    public static void topSort(ArrayList<Edge> graph[],int V){
+        boolean vis[] = new boolean[V]; 
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0;i<V;i++){
+            if(!vis[i]){
+                topSortUtil(graph,i,vis,stack);
+            }
+        }
+
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop() + " ");
+        }
+    }
+
+
 
     public static void main(String[] args) {
         int V = 6;
      
         ArrayList<Edge> graph[]  = new ArrayList[V];
         createGraph(graph);
+
+        topSort(graph,V);
     }
 }
